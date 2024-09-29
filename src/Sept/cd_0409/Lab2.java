b58f0b19b56a03595a73b67cc1da072904c66f92
Kanika-95 <sharmakanika5991@gmail.com>
2024-09-30 00:43:10 +0530

    Method overloading

--- src/Sept/cd_0409/Lab2.java
+++ src/Sept/cd_0409/Lab2.java
@@ -1,4 +1,14 @@
 package Sept.cd_0409;
 
+import java.sql.SQLOutput;
+
 public class Lab2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        test (args);
+    }
+    public static void test (String[] args)
+    {
+        System.out.println("How are you?");
+    }
 }
